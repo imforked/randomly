b457725e742c1fca72cb02e47f9c490c333638ec
imforked <villegasdominick@gmail.com>
2026-07-01 15:11:50 -0700

    Link to instagram

--- client/src/pages/Landing/LandingPage.tsx
+++ client/src/pages/Landing/LandingPage.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { FlippingText } from "src/components/FlippingLetterPool/FlippingText";
+import "./LandingPage.css";
 
 export function LandingPage() {
   return (
@@ -7,7 +8,17 @@ export function LandingPage() {
       <div className="stack-lg">
         <header className="stack">
           <FlippingText as="h1" text="Randomly" className="guide-prompt" />
-          <p className="subhead">By @100.11101</p>
+          <p className="subhead">
+            By{" "}
+            <a
+              href="https://www.instagram.com/100.11101/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="landing-page__credit-link"
+            >
+              @100.11101
+            </a>
+          </p>
         </header>
         <Link to="/create" className="btn">
           Start a Room
